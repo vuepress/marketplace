f190ed2dec60036bc6fc65389d22affcbefe6571
Mr.Hope <mister-hope@outlook.com>
2023-04-21 18:03:04 +0800

    chore: update deps

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,73 +1,76 @@
 import { hopeTheme } from "vuepress-theme-hope";
 
-export default hopeTheme({
-  hostname: "https://marketplacae.vuejs.press",
+export default hopeTheme(
+  {
+    hostname: "https://marketplacae.vuejs.press",
 
-  iconAssets: "fontawesome-with-brands",
+    iconAssets: "fontawesome-with-brands",
 
-  logo: "/logo.png",
+    logo: "/logo.png",
 
-  repo: "vuepress/marketplace",
+    repo: "vuepress/marketplace",
 
-  docsDir: "src",
+    docsDir: "src",
 
-  pageInfo: false,
+    pageInfo: false,
 
-  locales: {
-    "/": {
-      // navbar
-      navbar: ["/", "/plugins/", "/themes/"],
+    locales: {
+      "/": {
+        // navbar
+        navbar: ["/", "/plugins/", "/themes/"],
 
-      // sidebar
-      sidebar: "structure",
+        // sidebar
+        sidebar: "structure",
 
-      footer:
-        'Theme by <a href="https://theme-hope.vuejs.press/" target="_blank">VuePress Theme Hope</a> | CC0 Licensed',
+        footer:
+          'Theme by <a href="https://theme-hope.vuejs.press/" target="_blank">VuePress Theme Hope</a> | CC0 Licensed',
 
-      displayFooter: true,
+        displayFooter: true,
 
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
+        metaLocales: {
+          editLink: "Edit this page on GitHub",
+        },
+      },
+
+      /**
+       * Chinese locale config
+       */
+      "/zh/": {
+        // navbar
+        navbar: ["/zh/", "/zh/plugins/", "/zh/themes/"],
+
+        // sidebar
+        sidebar: "structure",
+
+        footer:
+          '主题使用 <a href="https://theme-hope.vuejs.press/" target="_blank">VuePress Theme Hope</a> | CC0 协议',
+
+        displayFooter: true,
+
+        // page meta
+        metaLocales: {
+          editLink: "在 GitHub 上编辑此页",
+        },
       },
     },
 
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: ["/zh/", "/zh/plugins/", "/zh/themes/"],
-
-      // sidebar
-      sidebar: "structure",
-
-      footer:
-        '主题使用 <a href="https://theme-hope.vuejs.press/" target="_blank">VuePress Theme Hope</a> | CC0 协议',
-
-      displayFooter: true,
-
-      // page meta
-      metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
+    plugins: {
+      comment: {
+        provider: "Giscus",
+        repo: "vuepress/marketplace",
+        repoId: "R_kgDOItkaoA",
+        category: "Announcements",
+        categoryId: "DIC_kwDOItkaoM4CTau2",
       },
+
+      mdEnhance: {
+        align: true,
+        codetabs: true,
+        tasklist: true,
+      },
+
+      photoSwipe: false,
     },
   },
-
-  plugins: {
-    comment: {
-      provider: "Giscus",
-      repo: "vuepress/marketplace",
-      repoId: "R_kgDOItkaoA",
-      category: "Announcements",
-      categoryId: "DIC_kwDOItkaoM4CTau2",
-    },
-
-    mdEnhance: {
-      align: true,
-      codetabs: true,
-      tasklist: true,
-    },
-
-    photoSwipe: false,
-  },
-});
+  false
+);
